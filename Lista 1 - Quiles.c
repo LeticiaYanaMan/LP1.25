9c687c56cef73dfffb4cd223f94c469381e90af0
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-04 10:55:22 -0300

    Create Lista 1 - Quiles.c

---
+++ Lista 1 - Quiles.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a <= b) {
+        int c = a*9;
+        int d = b*9;
+        int e = a*19;
+        int f = b*19;
+        
