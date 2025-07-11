940093fbaf920cdaadb2e75c5141b223d331b48f
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-11 20:52:17 -0300

    Create novaf1.c

---
+++ novaf1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+long char idade(int N) {
+  if (4 < N && N < 8) {
+    return 'Infantil A';
+  }
+  else if (7 < N && 11 > N) {
+    return 'Infantil B';
+  }
+ else if (10 < N && N > 14) {
+    return 'Juvenil A';
+ }
+ else if (13 < N && N > 18) {
+    return 'Juvenil B';
+ }
+
+
+
+
+int main() {
+  
