5f016252a7fdd9a856d278c5d13c672527fb92fb
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 16:13:14 -0300

    Update struct livros quiles.c

--- struct livros quiles.c
+++ struct livros quiles.c
@@ -27,18 +27,31 @@ struct Tlivro {
   float price;
 };
 
+
+
+int f1(char s1[M], char s2[M]) {
+  int igual = 1;
+  for (int i = 0; s1[i] != /0 && s2[i] != /0; i++) {     // /0 é quando chega no final - enquanto as duas ainda estão rolando: != ;0
+    if (s1[i] != s2[i]) {
+      igual = 0;
+      break;
+    }
+  }
+} // ele deu outra forma de chegar no mesmo resultado - tirei foto
+
 void imp_livro_edi (char ed[M]) {
   int l;
   for (l = 0; l < 500; l++) {
     if (strcmp(ed, livros[l].editora.nome) == 0) { // ou seja, sao iguais 
       printf("Titulo %s\n", livros[l].titulo 
-        
-
+    }
   }
 }
 
 int main() {
   imp_livro_edi("editoraX");
+
+
   return 0;
 }
   
