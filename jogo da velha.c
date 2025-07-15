ef002c2494a930b63e21c6cf6a4caeb657144969
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 13:52:49 -0300

    Update jogo da velha.c

--- jogo da velha.c
+++ jogo da velha.c
@@ -2,9 +2,17 @@
 
 int main() {
   for (int i = 0; i < 9; i++) {
-    int JOGO[3][3];
-    int Esq, Dir, val;
-    scanf("%d %d %d", &Esq, &Dir, &val);
+    int J[3][3];
+    int Linha, Coluna, val;
+    scanf("%d %d %d", &Linha, &Coluna, &val);
+    J[Linha][Coluna] = val;
+
+
+    printf("%d"
+  }
+
+  if (J[0][0] == J[0][1] == J[0][2] || J[1][0] == J[1][1] == J[1][2] || J[2][0] == J[2][1] == J[2][2]) {
+    
 
 
     
