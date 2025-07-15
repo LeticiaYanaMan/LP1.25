2a85eb92d3a7b9e7e980db04faaa0ce3dee1c663
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 11:15:07 -0300

    Create sudokuconfere.c

---
+++ sudokuconfere.c
@@ -0,0 +1,45 @@
+/* 
+  ter uma matriz com vários números - contar quantidade dentro de uma matriz
+*/
+
+#include <stdio.h>
+
+int VERIF(int M[n]) {
+  int V[10];
+  if (M[n] == 1) 
+    V[0] +=;
+  else if (M[n] == 2)
+    V[1] +=;
+  else if (M[n] == 3)
+    V[2] +=;
+  else if (M[n] == 4) 
+    V[3] +=;
+  else if (M[n] == 5) 
+    V[4] +=;
+  else if (M[n] == 6)
+    V[5] +=;
+  else if (M[n] == 7)
+    V[6] +=;
+  else if (M[n] == 8)
+    V[7] +=;
+  else if (M[n] == 9)
+    V[8] +=;
+}
+
+int main() {
+  int N; // quantos espaços precisam ser preenchidos
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) {
+    int VALOR[i];
+    scanf("%d", &VALOR[i]);
+    VERIF(VALOR[i]);
+    for (int j = 0; j < 9; j++) {
+      if (V[j] = 1) {
+
+
+
+
+
+
+
+
