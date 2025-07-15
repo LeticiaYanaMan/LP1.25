6a4af976dcc383a698a8527b701c60467019466a
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 17:03:45 -0300

    Update jogo da velha.c

--- jogo da velha.c
+++ jogo da velha.c
@@ -1,49 +1,22 @@
-#include <stdio.h>
-
-char J[3][3];
-
-
-char inicio(int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      return J[i][j] = '_';
-    }
-  }
-}
-
-char printjogo(int n) {
-  for (int i = 0; i < n; i++) {
-    for (int i = 0; i < n; i++) {
-      return printf("%c", J[i][j]);
-    }
-  }
-}
-
-char colojogo(int l, int c, char t) {
-  if (J[l][c] = '_') {
-    J[l][c] = t;
-    return printjogo(3);
-  }
-  if (J[l][c] = 'x' || J[l][c] = 'o') {
-    return printf("já ocupado\n");
-  }
-}
+include <stdio.h>
+#include <string.h>
 
 int main() {
-  int n = 3;
-  inicio(n);
-  for (int i = 0; i < 1001; i++) {
-    int l, c;
-    char t;
-    scanf("%d %d %c", &l, %c, %t);
-    colojogo(l, c, t);
-  }
-  return 0;
-}
-
-  
-
-  
-
-
+    char TRE[3][3];
     
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            TRE[i][j] = '_';
+        }
+    }
+
+    for(int i = 0; i < 3; i++) {
+        printf("\t");
+        for(int j = 0; j < 3; j++) {
+            printf("%c", TRE[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
