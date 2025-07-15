166e6c4cc3206fbfc4efd3b11713eea6f2355b35
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 16:43:31 -0300

    Update string_palindromo.c

--- string_palindromo.c
+++ string_palindromo.c
@@ -1,3 +1,5 @@
+// uma palavra, sem espacamento
+
 #include <stdio.h>
 #include <string.h>
 
@@ -22,3 +24,39 @@ int main() {
   
   return 0;
 }
+
+// com espacamento (fgets)
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char STR[1001];
+  
+  fgets(STR, 1001, stdin);
+  STR[strlen(STR) -1] = '\0'; 
+  
+  char STRaux[1001];
+  for (int i = 0; i < 1001; i++) {
+    STRaux[i] = STR[i];
+  }
+
+  size_t len = strlen(STR);
+
+  for (int i = 0; i < len; i++) {
+    STR[i] = STRaux[len-1 -i];
+  }
+
+  for (int i = 0; i < len; i++) {
+     printf("%c", STR[i]);
+  }
+  
+  return 0;
+}
+
+
+
+
+
+
+
