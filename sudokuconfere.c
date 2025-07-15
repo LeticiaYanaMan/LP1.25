52ed14dfa671f5f1d4f35b7d6f8585dcd84103ec
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 11:35:43 -0300

    Update sudokuconfere.c

--- sudokuconfere.c
+++ sudokuconfere.c
@@ -1,40 +1,59 @@
 /* 
   ter uma matriz com vários números - contar quantidade dentro de uma matriz
-*/
+*/ 
+
 
 #include <stdio.h>
 
-int VERIF(int M[n]) {
+int VERIF(int M) {
   int V[10];
-  if (M[n] == 1) 
-    V[0] +=;
-  else if (M[n] == 2)
-    V[1] +=;
-  else if (M[n] == 3)
-    V[2] +=;
-  else if (M[n] == 4) 
-    V[3] +=;
-  else if (M[n] == 5) 
-    V[4] +=;
-  else if (M[n] == 6)
-    V[5] +=;
-  else if (M[n] == 7)
-    V[6] +=;
-  else if (M[n] == 8)
-    V[7] +=;
-  else if (M[n] == 9)
-    V[8] +=;
+  if (M == 1) {
+    V[0] += 1;
+  }
+  else if (M == 2) {
+    V[1] += 1;
+  }
+  else if (M == 3) {
+    V[2] += 1;
+  }
+  else if (M == 4) {
+    V[3] += 1;
+  }
+  else if (M == 5) { 
+    V[4] += 1;
+  }
+  else if (M == 6) {
+    V[5] += 1;
+  }
+  else if (M == 7) {
+    V[6] += 1;
+  }
+  else if (M == 8) {
+    V[7] += 1;
+  }
+  else if (M == 9) {
+    V[8] += 1;
+  }
 }
 
 int main() {
-  int N; // quantos espaços precisam ser preenchidos
+  int N;
+  int V[10]; // quantos espaços precisam ser preenchidos
   scanf("%d", &N);
   for (int i = 0; i < N; i++) {
     int VALOR[i];
     scanf("%d", &VALOR[i]);
     VERIF(VALOR[i]);
-    for (int j = 0; j < 9; j++) {
-      if (V[j] = 1) {
+    for (int j = 0; j < 10; j++) {
+      if (V[j] != 0 && V[j] != 1) {
+        printf("ERROR\n");
+        return 0;
+      }
+    }
+    printf("%d", VALOR[i]);
+  }
+  return 0;
+}
 
 
 
