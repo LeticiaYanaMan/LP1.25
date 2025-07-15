7f17c5b47556ed15cb872c7dd68e32b26a654acc
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 10:56:17 -0300

    Update palindromo.c

--- palindromo.c
+++ palindromo.c
@@ -6,20 +6,19 @@ int main() {
   scanf("%1000s", STR);
   char STRaux[1001];
   for (int i = 0; i < 1001; i++) {
-    STRaux[1001] = STR[1001];
+    STRaux[i] = STR[i];
   }
   // STRaux tem a mesma palavra que STR 
 
   size_t len = strlen(STR);
-  // 
-  for (int i = 0; i < (len + 1)/2; i++) {
-    STR[i] = STRaux[len-i];
+
+  for (int i = 0; i < len; i++) {
+    STR[i] = STRaux[len-1 -i];
   }
 
-  for (int i = 0; i < len + 1; i++) {
+  for (int i = 0; i < len; i++) {
      printf("%c", STR[i]);
   }
   
   return 0;
 }
-  
