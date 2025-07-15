bcb2ba588b42b069085a2640f09e9970988353b3
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-15 10:40:25 -0300

    Create palindromo.c

---
+++ palindromo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char STR[1001];
+  scanf("%1000s", STR);
+  char STRaux[1001];
+  for (int i = 0; i < 1001; i++) {
+    STRaux[1001] = STR[1001];
+  }
+
+
+  // STRaux tem a mesma palavra que STR 
+  for (int i = 0; i < 500; i++) {
+    STR[i] =  
+  
